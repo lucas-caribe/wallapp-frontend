3eac1779c8b6fdd98f67219ea128cf572411a285
Lucas Caribé <lucasmoc47@gmail.com>
2021-11-16 09:46:37 -0300

    Fixing loading behavior and styling the scrollbar

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -10,20 +10,14 @@ import './style.css';
 function PostList() {
   const { postList, isFetching } = useContext(PostContext);
 
-  if (!postList.length) {
-    return (
-      <div className="post-list">
-        <h4 className="no-posts">No posts yet...</h4>
-      </div>
-    );
-  }
-
   return (
     <div className="post-list">
       {isFetching && <Loading />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {postList.length ? (
+        postList.map((post) => <Post key={post.id} post={post} />)
+      ) : (
+        <h4 className="no-posts">No posts yet...</h4>
+      )}
     </div>
   );
 }
